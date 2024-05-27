4acff6460bf603325c95f66f9ce6b06a531c57fe
fadhilrobbani <rabbanifadhillailham@gmail.com>
2024-05-27 12:24:31 +0700

    test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = require("./app");
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || "localhost";
 
+//listen
 app.listen(port, host, () => {
   console.log(`Listening: http://${host}:${port}`);
 });
