c801f8f9250c395d5db6c81cd431054bf72b19ad
fadhilrobbani <rabbanifadhillailham@gmail.com>
2024-06-01 23:27:42 +0700

    add cloud storage package and config

---
+++ src/config/storage.js
@@ -0,0 +1,49 @@
+const { Storage } = require("@google-cloud/storage");
+const fs = require("fs");
+const dateFormat = require("dateformat");
+const path = require("path");
+require("dotenv").config();
+
+const pathKey = path.resolve("../serviceaccountkey.json");
+
+const gcs = new Storage({
+  projectId: process.env.GCP_PROJECT_ID,
+  keyFilename: pathKey,
+});
+
+const bucketName = process.env.GCS_BUCKET_NAME;
+const bucket = gcs.bucket(bucketName);
+
+// function getPublicUrl(filename) {
+//    return 'https://storage.googleapis.com/' + bucketName + '/' + filename;
+// }
+
+// let ImgUpload = {}
+
+// ImgUpload.uploadToGcs = (req, res, next) => {
+//    if (!req.file) return next()
+
+//    const gcsname = dateFormat(new Date(), "yyyymmdd-HHMMss")
+//    const file = bucket.file(gcsname)
+
+//    const stream = file.createWriteStream({
+//        metadata: {
+//            contentType: req.file.mimetype
+//        }
+//    })
+
+//    stream.on('error', (err) => {
+//        req.file.cloudStorageError = err
+//        next(err)
+//    })
+
+//    stream.on('finish', () => {
+//        req.file.cloudStorageObject = gcsname
+//        req.file.cloudStoragePublicUrl = getPublicUrl(gcsname)
+//        next()
+//    })
+
+//    stream.end(req.file.buffer)
+// }
+
+module.exports = { bucket, bucketName, gcs };
