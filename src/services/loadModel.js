579f9fd234621762263cc521ae126429cf0e8ee0
fadhilrobbani <rabbanifadhillailham@gmail.com>
2024-06-09 16:10:41 +0700

    add model

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -6,15 +6,15 @@ const path = require("path");
 
 // const loadModel = async () =>
 //   await tf.loadLayersModel("file://model/model.json");
-// const loadModel = async () => await tf.loadLayersModel(process.env.MODEL_URL);
+const loadModel = async () => await tf.loadLayersModel(process.env.MODEL_URL);
 // const loadModel = async () => {
 //   const modelJson = fs.readFileSync(path.join(__dirname, modelPath), "utf8");
 //   const modelBuffer = Buffer.from(modelJson, "utf8");
 //   return await tf.loadLayersModel(tf.io.fromMemory(modelBuffer));
 // };
-const loadModel = async () => {
-  const handler = tf.io.fileSystem(path.join(__dirname, modelPath));
-  return await tf.loadLayersModel(handler);
-};
+// const loadModel = async () => {
+//   const handler = tf.io.fileSystem(path.join(__dirname, modelPath));
+//   return await tf.loadLayersModel(handler);
+// };
 
 module.exports = loadModel;
